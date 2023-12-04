04f1dee932602d13cecc8f7737e7530a2f01499e
remtori <lqvu99@gmail.com>
2023-12-05 00:08:25 +0700

    yeet all the old/unused code

--- src/platform/default-script-template.ts
+++
@@ -1,9 +0,0 @@
-export const meta: Meta = {
-	description: 'Read A and B from user input, then output the result of A + B',
-};
-
-export const main: Main = async (ctx) => {
-	const a = ctx.readInt('input A');
-	const b = ctx.readFloat('input B', { max: 10 });
-	ctx.writeln('the answer is', a + b);
-}
