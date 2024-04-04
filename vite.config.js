73827e7956d8215ad6e0a436794a6b5d19ab694c
Quang Vu <vulq2@vng.com.vn>
2024-04-04 14:08:53 +0700

    use relative import for assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+	base: '',
 	plugins: [
 		solidPlugin(),
 		tsconfigPaths()
